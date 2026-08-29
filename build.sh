068ae252ec22c1bfbf838279416a84b70d1aff71
Diocebar Zorrilla <diocebarandres@gmail.com>
2026-08-29 13:41:52 -0400

    .

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install dependencies
 pip install -r requirements/production.txt
 
-# Convert static asset files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
 python manage.py migrate
 
-# Create initial superuser automatically if none exists
 python manage.py crear_superusuario_inicial
