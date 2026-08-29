c467203bd882f6109b780ed2b96504a4bdb0f125
Diocebar Zorrilla <diocebarandres@gmail.com>
2026-08-28 22:04:33 -0400

    Mejoras en panel de estudiante y deshabilitacion de validators de password

--- config/settings/base.py
+++ config/settings/base.py
@@ -108,19 +108,19 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 
 # Validadores de contrasena
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': {'min_length': 5},
+        'OPTIONS': {'min_length': 4},
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 # Django REST Framework
